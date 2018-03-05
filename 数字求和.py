225bf5a56763269fca12a3672f723dcfcc12a159
Sometimes-tang <35586099+Sometimes-tang@users.noreply.github.com>
2018-03-05 08:05:10 -0600

    Add files via upload
    https://m.runoob.com/python3/python3-add-number.html 实例

---
+++ 数字求和.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# In[15]:
+# https://m.runoob.com/python3/python3-add-number.html 实例
+
+
+num1 = input('输入第一个数字')
+num2 = input('输入第二个数字')
+sum = float(num1) + float(num2)
+multiplicative = float(num1) * float(num2)
+print('数字{0}和{1}相加结果为：{2}'.format(num1, num2, sum))
+print('数字{0}和{1}相乘结果为：{2}'.format(num1, num2, multiplicative))
+
